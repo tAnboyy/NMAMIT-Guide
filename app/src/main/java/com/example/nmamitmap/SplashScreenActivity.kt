353ef01cb5d5e8a6def63bdf344f4eb05afb7dee
Thanmay M Das <mdasthanmay@gmail.com>
2022-08-26 17:29:18 +0530

    Add splash screen

---
+++ app/src/main/java/com/example/nmamitmap/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.nmamitmap
+
+import android.app.ActivityOptions
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val logo: ImageView = findViewById(R.id.iv_splash_logo)
+        val anim = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        logo.startAnimation(anim)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MapsActivity::class.java), ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+            finish()
+        }, 2000)
+    }
+}
